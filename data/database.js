6eea6e9947c61150c14dd6af635eb47c9a94f823
Richard <ogb22001@byui.edu>
2025-10-04 20:26:45 +0100

    project2 incoming...

---
+++ data/database.js
@@ -0,0 +1,31 @@
+const dotenv = require('dotenv');
+dotenv.config();
+// console.log('DEBUG -> MONGODB_URL value:', process.env.MONGODB_URL);
+
+const MongoClient = require('mongodb').MongoClient;
+
+let database;
+
+const initDb = (callback) => {
+    if (database) {
+        console.log('Database is initialized!');
+        return callback(null, database);
+    }
+    MongoClient.connect(process.env.MONGODB_URL)
+        .then((client) => {
+            database = client.db();
+            callback(null, database);
+        })
+        .catch((err) => {
+            callback(err);
+        });
+}
+
+const getDatabase = () => {
+    if (!database) {
+        throw Error('Database not initialized');
+    }
+    return database;
+}
+
+module.exports = { initDb, getDatabase };
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const mongodb = require('./data/database');
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {
+            console.log(`Database is listening and Server is running on port: ${port}`)
+        });
+    }
+});
